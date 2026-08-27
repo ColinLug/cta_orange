d2ca19b0ea0f339e3d951a41f6751ce22c1289e4
ColinLug <colin.lugin@gmail.com>
2026-08-28 00:23:54 +0200

    Update test_segmentation.py

---
+++ tests/widgets/test_segmentation.py
@@ -0,0 +1,50 @@
+import pytest
+from Orange.widgets.tests.base import WidgetTest
+from unittest.mock import MagicMock
+
+from cta_orange.widgets.segmentation import CTASegmentation
+
+class TestSegWidget(WidgetTest):
+    """Tests for CTASegmentation."""
+
+    def setUp(self):
+        """Prepare for the test"""
+        # Création du widget avec les helpers Orange
+        self.widget = self.create_widget(CTASegmentation)
+        self.widget.error = MagicMock()
+        self.widget.information = MagicMock()
+
+    def test_change_mode(self):
+        """Test if changing mode works"""
+        self.widget.delimiterLine.setVisible = MagicMock()
+        self.widget.mode = "delimiter"
+        self.widget.changeMode()
+        self.widget.delimiterLine.setVisible.assert_called_with(True)
+
+        self.widget.mode = "chars"
+        self.widget.changeMode()
+        self.widget.delimiterLine.setVisible.assert_called_with(False)
+
+    def test_handle_new_signals_missing_input(self):
+        """Test handleNewSignals when input is missing"""
+        self.widget.ref = None
+        self.widget._logic.session = MagicMock()
+        result = self.widget.handleNewSignals()
+        self.assertIsNone(result)
+        self.widget.error.assert_called_with("No upstream data connected.")
+
+        self.widget.ref = MagicMock()
+        self.widget._logic.session = None
+        result = self.widget.handleNewSignals()
+        self.assertIsNone(result)
+        self.widget.error.assert_called_with("No upstream data connected.")
+
+    def test_handle_new_signals_missing_delimiter(self):
+        """Test handleNewSignals when in delimiter mode and delimiter is missing"""
+        self.widget.delimiter = ""
+        self.widget.mode = "delimiter"
+        self.widget.ref = MagicMock()
+        self.widget._logic.session = MagicMock()
+        result = self.widget.handleNewSignals()
+        self.assertIsNone(result)
+        self.widget.error.assert_called_with("The delimiter is empty, please use \"chars\" mode or provide a legitimate delimiter.")
