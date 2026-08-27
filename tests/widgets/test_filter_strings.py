112a668253a5f84f623e30edba06f8a077e18160
ColinLug <colin.lugin@gmail.com>
2026-08-27 23:44:56 +0200

    Update test_filter_strings.py

---
+++ tests/widgets/test_filter_strings.py
@@ -0,0 +1,40 @@
+import pytest
+from Orange.widgets.tests.base import WidgetTest
+from unittest.mock import MagicMixin, MagicMock
+
+from cta_orange.widgets.filter_strings import StringsFilter
+
+
+
+class TestFilterWidget(WidgetTest):
+    """Tests for StringsFilter."""
+
+    def setUp(self):
+        """Prepare for the test"""
+        # Création du widget avec les helpers Orange
+        self.widget = self.create_widget(StringsFilter)
+        self.widget.error = MagicMock()
+        self.widget.information = MagicMock()
+
+    def test_handle_new_signals_missing_input(self):
+        """Test handleNewSignals when input is missing"""
+        self.widget.ref = None
+        self.widget._logic.session = MagicMock()
+        result = self.widget.handleNewSignals()
+        self.assertIsNone(result)
+        self.widget.error.assert_called_with("No upstream data connected.")
+
+        self.widget.ref = MagicMock()
+        self.widget._logic.session = None
+        result = self.widget.handleNewSignals()
+        self.assertIsNone(result)
+        self.widget.error.assert_called_with("No upstream data connected.")
+
+    def test_predicate_empty(self):
+        """Test handleNewSignals when predicate is empty"""
+        self.widget.ref = MagicMock()
+        self.widget._logic.session = MagicMock()
+        self.widget.predicate = ""
+        result = self.widget.handleNewSignals()
+        self.assertIsNone(result)
+        self.widget.information.assert_called_with("No predicate configured yet.")
